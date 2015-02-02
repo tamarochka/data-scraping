18907f2334cb4804a440c1bc20a21ad6d7cd7017
tamarochka <tamara.zenkevich@gmail.com>
2015-02-02 00:23:13 -0500

    fixed spaces

--- data-reviews1.rb
+++ data-reviews1.rb
@@ -1,21 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 require "Pry"
-	url = 'http://www.amazon.com/46894A-Programmable-Coffeemaker-Frustration-Packaging/product-reviews/B00EI7DPTY/ref=cm_cr_pr_top_link_next_2?ie=UTF8&pageNumber=1'
-	page = Nokogiri::HTML(open(url))
-	good_reviews = []
-	#table productReviews tbody
-	reviews = page.xpath("//tr/td/div/div")
-
-		reviews.each do |review|
-			if review.content.include? "1.0 out of 5 stars"
-				good_reviews << review
-			end
-		end
 
 
-	#   binding.pry
-	# File.new("reviews.txt", "a")
+url = 'http://www.amazon.com/46894A-Programmable-Coffeemaker-Frustration-Packaging/product-reviews/B00EI7DPTY/ref=cm_cr_pr_top_link_next_2?ie=UTF8&pageNumber=1'
+page = Nokogiri::HTML(open(url))
+good_reviews = []
+reviews = page.xpath("//tr/td/div/div")
+
+reviews.each do |review|
+	if review.content.include? "1.0 out of 5 stars"
+		good_reviews << review
+	end
+end
 
 File.open("reviews.txt", "a") do |i|
 	good_reviews.each do |link|
